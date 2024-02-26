ea9b3c9ea5c4bd3b58d9473918d00c650fc314fb
MiShOnY5530 <alex.zasadiuk@ukr.net>
2024-02-26 22:58:53 +0200

    add

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ $(document).ready(function () {
     .then(response => response.json())
     .then(json => console.log(json));
   }
+
+
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+
+function sendRequestUser (){
+  return fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json => console.log(json));
+}
+
