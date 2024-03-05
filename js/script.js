46cfee91ac58e41757a02d7569a63ae15652b09f
MiShOnY5530 <alex.zasadiuk@ukr.net>
2024-03-05 19:49:20 +0200

    add script

--- js/script.js
+++ js/script.js
@@ -1,65 +1,15 @@
-$(document).ready(function () {
-    $("#news-slider").owlCarousel({
-      items: 3,
-      itemsDesktop: [1199, 3],
-      itemsDesktopSmall: [980, 2],
-      itemsMobile: [600, 1],
-      navigation: true,
-      navigationText: ["", ""],
-      pagination: true,
-      autoPlay: true
-    });
-  });
-  
+const name = "Alex"
+let fullName = "Alex Zasadiuk"
+var age = "29"
 
-  function sendRequest (){
-    return fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => response.json())
-    .then(json => console.log(json));
-  }
+console.log(name)
+console.log(fullName)
+console.log(age)
 
 
-  // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict'
+const strinName = "123"
+const boolNameFalse = false;
+const boolNameTrue = true;
+const variableUndefined = undefined;
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
-
-
-function sendRequestUser (){
-  return fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(json => console.log(json));
-}
-
-function checkedValid() {
-  const formContent = document.getElementById("form-extention");
-  return formContent.classList.toggle("was-validated");
-}
-
-function showModals(){
-  alert("hi")
-  prompt("enter")
-  confirm("holly")
-}
-
-console.log ('Hi log')
-
-function showConsole(){
-  console.log ('Я учень IT')
-}
+const variableNull = null
